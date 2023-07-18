4e33c8f317f5d139bc23d3432ae995b056806368
Kiril <ikera009@gmail.com>
2023-07-18 13:41:40 +0300

    chore: Fix lint warnings

--- src/deals/deals.service.ts
+++ src/deals/deals.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { catchError, firstValueFrom, Observable } from 'rxjs';
-import { AxiosError, AxiosResponse } from 'axios';
+import { catchError, firstValueFrom } from 'rxjs';
+import { AxiosError } from 'axios';
 import { DealDto } from './dto/deal.dto';
 
 @Injectable()
