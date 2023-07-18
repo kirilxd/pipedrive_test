f56358d27bbad9ea46f6a5448b72ebf4dea0d194
Kiril <ikera009@gmail.com>
2023-07-18 13:25:40 +0300

    chore: Add metrics endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DealsModule } from './deals/deals.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DealsModule],
+  imports: [ConfigModule.forRoot(), DealsModule, PrometheusModule.register()],
   controllers: [AppController],
   providers: [AppService],
 })
